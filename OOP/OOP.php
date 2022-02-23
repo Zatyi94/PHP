e174e7118545518809861065cf02df68719ffdb4
Zatyi94 <zatyko.nora@gmail.com>
2022-02-23 19:43:58 +0100

    implemented helloName and rectagleArea functions

--- OOP/OOP.php
+++ OOP/OOP.php
@@ -5,3 +5,26 @@ function hello(){
 }
 
 hello();
+
+function helloName(string $name){
+    // echo gettype($name);
+    echo "Hello $name!".PHP_EOL;
+}
+
+helloName("Anna");
+helloName("Péter");
+helloName("Krisztián");
+
+echo PHP_EOL;
+echo PHP_EOL;
+
+function rectangleArea(float $a, float $b):float{
+    return $a * $b;
+}
+
+echo "Téglalp (3,4) területe: " . rectangleArea(3, 4).PHP_EOL;
+echo "Téglalp (3,5.3) területe: " . rectangleArea(3, 5.3).PHP_EOL;
+echo "Téglalp (3,10) területe: " . rectangleArea(3, 10).PHP_EOL;
+
+echo PHP_EOL;
+echo PHP_EOL;
